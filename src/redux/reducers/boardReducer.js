8c97f42d0fc556861e8b2ad079037aea489a0a8a
drmeloy <drmeloy@gmail.com>
2020-05-12 18:32:00 -0700

    beginning board reducer TDD

--- src/redux/reducers/boardReducer.js
+++ src/redux/reducers/boardReducer.js
@@ -1,14 +1,24 @@
+import { CHECK_RED } from '../actions/boxActions';
+
 const initialState = {
-  reds: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
-  yellows: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
-  greens: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
-  blues: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
-  penalties: [],
+  red: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+  yellow: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+  green: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+  blue: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+  penalty: [],
   score: 0
+};
+
+const generateBoxArray = num => {
+  const array = [];
+  for(let i = num + 1; i < 13; i++) array.push(i);
+  return array;
 };
 
 export default function reducer(state = initialState, action){
   switch(action.type){
-    case:
+    case CHECK_RED: 
+      const boxArray = generateBoxArray(action.payload);
+      return { ...state, red: boxArray };
   }
 }
---
+++ src/redux/reducers/boardReducer.test.js
@@ -0,0 +1,13 @@
+import { checkRed } from '../actions/boxActions';
+import reducer from './boardReducer';
+
+describe('boxes reducer', () => {
+  it('handles a checkRed action', () => {
+    const action = checkRed(4);
+    const initialState = { red: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12] };
+    const newState = reducer(initialState, action);
+    expect(newState).toEqual({ red: [5, 6, 7, 8, 9, 10, 11, 12] });
+    const newState2 = reducer(newState, checkRed(8));
+    expect(newState2).toEqual({ red: [9, 10, 11, 12] });
+  });
+});
