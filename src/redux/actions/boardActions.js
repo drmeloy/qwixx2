81492c440e1ef8c01eaa1b7123176cc7083b6ffe
drmeloy <drmeloy@gmail.com>
2020-05-12 18:14:52 -0700

    board actions and tests written

--- src/redux/actions/boardActions.js
+++ src/redux/actions/boardActions.js
@@ -1,2 +1,14 @@
 export const CHECK_RED = 'CHECK_RED';
 export const checkRed = num => ({ type: CHECK_RED, payload: num });
+
+export const CHECK_YELLOW = 'CHECK_YELLOW';
+export const checkYellow = num => ({ type: CHECK_YELLOW, payload: num });
+
+export const CHECK_GREEN = 'CHECK_GREEN';
+export const checkGreen = num => ({ type: CHECK_GREEN, payload: num });
+
+export const CHECK_BLUE = 'CHECK_BLUE';
+export const checkBlue = num => ({ type: CHECK_BLUE, payload: num });
+
+export const CHECKK_PENALTY = 'CHECK_PENALTY';
+export const checkPenalty = () => ({ type: CHECKK_PENALTY });
--- src/redux/actions/boardActions.test.js
+++ src/redux/actions/boardActions.test.js
@@ -1,8 +1,28 @@
-import { CHECK_RED, checkRed } from './boardActions';
+import { CHECK_RED, checkRed, CHECK_YELLOW, checkYellow, CHECK_GREEN, checkGreen, CHECK_BLUE, checkBlue, CHECKK_PENALTY, checkPenalty } from './boardActions';
 
 describe('board action', () => {
   it('can create a CHECK_RED action', () => {
     const action = checkRed(2);
     expect(action).toEqual({ type: CHECK_RED, payload: 2 });
+  });
+
+  it('can create a CHECK_YELLOW action', () => {
+    const action = checkYellow(4);
+    expect(action).toEqual({ type: CHECK_YELLOW, payload: 4 });
+  });
+
+  it('can create a CHECK_GREEN action', () => {
+    const action = checkGreen(11);
+    expect(action).toEqual({ type: CHECK_GREEN, payload: 11 });
+  });
+
+  it('can create a CHECK_BLUE action', () => {
+    const action = checkBlue(12);
+    expect(action).toEqual({ type: CHECK_BLUE, payload: 12 });
+  });
+
+  it('can create a CHECK_PENALTY action', () => {
+    const action = checkPenalty();
+    expect(action).toEqual({ type: CHECKK_PENALTY});
   });
 });
