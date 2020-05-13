eecfa2856c46116ccd77cfbf859ed7e931a1d1ec
drmeloy <drmeloy@gmail.com>
2020-05-12 18:50:20 -0700

    boxesReducer and tests written

--- src/redux/reducers/boxesReducer.js
+++ src/redux/reducers/boxesReducer.js
@@ -1,12 +1,11 @@
-import { CHECK_RED } from '../actions/boxActions';
+import { CHECK_RED, CHECK_YELLOW, CHECK_GREEN, CHECK_BLUE, CHECKK_PENALTY } from '../actions/boxActions';
 
 const initialState = {
   red: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
   yellow: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
   green: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
   blue: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
-  penalty: [],
-  score: 0
+  penalties: 0
 };
 
 const generateBoxArray = num => {
@@ -16,9 +15,17 @@ const generateBoxArray = num => {
 };
 
 export default function reducer(state = initialState, action){
+  const boxArray = generateBoxArray(action.payload);
   switch(action.type){
     case CHECK_RED: 
-      const boxArray = generateBoxArray(action.payload);
       return { ...state, red: boxArray };
+    case CHECK_YELLOW:
+      return { ...state, yellow: boxArray };
+    case CHECK_GREEN:
+      return { ...state, green: boxArray };
+    case CHECK_BLUE:
+      return { ...state, blue: boxArray };
+    case CHECKK_PENALTY:
+      return { ...state, penalties: state.penalties + 1 };
   }
 }
--- src/redux/reducers/boxesReducer.test.js
+++ src/redux/reducers/boxesReducer.test.js
@@ -1,13 +1,52 @@
-import { checkRed } from '../actions/boxActions';
+import { checkRed, checkYellow, checkGreen, checkBlue, checkPenalty } from '../actions/boxActions';
 import reducer from './boxesReducer';
 
 describe('boxes reducer', () => {
   it('handles a checkRed action', () => {
-    const action = checkRed(4);
     const initialState = { red: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12] };
-    const newState = reducer(initialState, action);
+
+    const newState = reducer(initialState, checkRed(4));
     expect(newState).toEqual({ red: [5, 6, 7, 8, 9, 10, 11, 12] });
+
     const newState2 = reducer(newState, checkRed(8));
     expect(newState2).toEqual({ red: [9, 10, 11, 12] });
+
+    const newState3 = reducer(newState2, checkRed(12));
+    expect(newState3).toEqual({ red: [] });
+  });
+
+  it('handles a checkYellow action', () => {
+    const initialState = { yellow: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12] };
+
+    const newState = reducer(initialState, checkYellow(2));
+    expect(newState).toEqual({ yellow: [3, 4, 5, 6, 7, 8, 9, 10, 11, 12] });
+
+    const newState2 = reducer(newState, checkYellow(3));
+    expect(newState2).toEqual({ yellow: [4, 5, 6, 7, 8, 9, 10, 11, 12] });
+
+    const newState3 = reducer(newState2, checkYellow(11));
+    expect(newState3).toEqual({ yellow: [12] });
+  });
+
+  it('handles a checkGreen action', () => {
+    const initialState = { green: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12] };
+    const newState = reducer(initialState, checkGreen(5));
+    expect(newState).toEqual({ green: [6, 7, 8, 9, 10, 11, 12] });
+  });
+
+  it('handles a checkBlue action', () => {
+    const initialState = { blue: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12] };
+    const newState = reducer(initialState, checkBlue(10));
+    expect(newState).toEqual({ blue: [11, 12] });
+  });
+
+  it('handles a checkPenalty action', () => {
+    const initialState = { penalties: 0 };
+
+    const newState = reducer(initialState, checkPenalty());
+    expect(newState).toEqual({ penalties: 1 });
+
+    const newState2 = reducer(newState, checkPenalty());
+    expect(newState2).toEqual({ penalties: 2 });
   });
 });
