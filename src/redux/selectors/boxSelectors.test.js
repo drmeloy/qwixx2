b9c6f8a52af2bab3090a48b87ea9f2204a92aa39
drmeloy <drmeloy@gmail.com>
2020-05-14 11:44:53 -0700

    box selectors and tests written

---
+++ src/redux/selectors/boxSelectors.test.js
@@ -0,0 +1,31 @@
+import { getReds, getYellows, getGreens, getBlues, getPenalties } from './boxSelectors';
+
+describe('box selectors', () => {
+  const state = {
+    red: [5, 6, 7, 8, 9, 10, 11, 12],
+    yellow: [],
+    green: [9, 10, 11, 12],
+    blue: [3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+    penalties: 2
+  };
+
+  it('can get the reds', () => {
+    expect(getReds(state)).toEqual([5, 6, 7, 8, 9, 10, 11, 12]);
+  });
+
+  it('can get the yellows', () => {
+    expect(getYellows(state)).toEqual([]);
+  });
+
+  it('can get the greens', () => {
+    expect(getGreens(state)).toEqual([9, 10, 11, 12]);
+  });
+
+  it('can get the blues', () => {
+    expect(getBlues(state)).toEqual([3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
+  });
+
+  it('can get the penalties', () => {
+    expect(getPenalties(state)).toEqual(2);
+  });
+});
