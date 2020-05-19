67a7534900eb07debecb2c2287ef55bbe5abc670
drmeloy <drmeloy@gmail.com>
2020-05-19 12:53:17 -0700

    roll dice dispatch working properly

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import styles from './Board.css';
-import { useDispatch } from 'react-redux';
-import { getRedDieValue } from '../../redux/selectors/diceSelectors';
+import { useDispatch, useSelector } from 'react-redux';
+import { getRedDieValue, getBlueDieValue } from '../../redux/selectors/diceSelectors';
+import { rollDice } from '../../redux/actions/diceActions';
 
 export default function Board() {
+  const dispatch = useDispatch();
 
   return (
     <>
-      <div className={styles.Board}>Hello</div>
+      <div className={styles.Board}>
+        <button onClick={() => dispatch(rollDice())}>Roll</button>
+      </div>
     </>
   );
 }
