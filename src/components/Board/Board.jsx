ff1c6b67babdc77c6340e49a4595ad99632b395b
drmeloy <drmeloy@gmail.com>
2020-05-19 13:27:12 -0700

    updated Board test with Provider wrapper

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import styles from './Board.css';
 import { useDispatch, useSelector } from 'react-redux';
-import { getRedDieValue, getBlueDieValue } from '../../redux/selectors/diceSelectors';
+import { getWhiteDie1Value, getWhiteDie2Value, getRedDieValue, getYellowDieValue, getGreenDieValue, getBlueDieValue } from '../../redux/selectors/diceSelectors';
 import { rollDice } from '../../redux/actions/diceActions';
 
 export default function Board() {
   const dispatch = useDispatch();
+  const white1 = useSelector(getWhiteDie1Value);
+  const white2 = useSelector(getWhiteDie2Value);
+  const red = useSelector(getRedDieValue);
+  const yellow = useSelector(getYellowDieValue);
+  const green = useSelector(getGreenDieValue);
+  const blue = useSelector(getBlueDieValue);
 
   return (
     <>
-      <div className={styles.Board}>
-        <button onClick={() => dispatch(rollDice())}>Roll</button>
-      </div>
+      <button onClick={() => dispatch(rollDice())}>Roll</button>
+      <div>{white1}</div>
+      <div>{white2}</div>
+      <div>{red}</div>
+      <div>{yellow}</div>
+      <div>{green}</div>
+      <div>{blue}</div>
     </>
   );
 }
