41a41529c4c9c1d11b25976ebcff813907ac9cb4
drmeloy <drmeloy@gmail.com>
2020-05-25 11:00:05 -0700

    refactored NumberBoxes.jsx

--- src/components/NumberBoxes/NumberBoxes.jsx
+++ src/components/NumberBoxes/NumberBoxes.jsx
@@ -1,29 +1,8 @@
 import React from 'react';
 import styles from './NumberBoxes.css';
-import lock from '../../../public/assets/lock.png';
+import { boxes } from '../../utils/boxesHelpers';
 
 export default function NumberBoxes(){
-  const colors = ['red', 'yellow', 'green', 'blue'];
-  const generateRow = color => {
-    let numbers = [];
-    if(color === 'red' || color === 'yellow'){
-      for(let i = 2; i < 14; i++){
-        numbers.push(i);
-      }
-    }
-    else if(color === 'green' || color === 'blue'){
-      for(let i = 12; i > 0; i--){
-        numbers.push(i);
-      }
-    }
-    return numbers.map((num, i) => (
-      <li key={color + ' ' + i} className={styles[color]}>
-        {num === 13 || num === 1 ? <img src={lock}></img> : num}
-      </li>
-    ));
-  };
-  const boxes = colors.map(color => generateRow(color));
-
   return (
     <section className={styles.NumberBoxes}>
       <ul>
