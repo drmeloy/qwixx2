71f57ed9f42d245229410f5a9fae217e41883004
drmeloy <drmeloy@gmail.com>
2020-06-01 17:27:34 -0700

    added actionsReducer into mainReducer, putting it into state

--- src/redux/reducers/mainReducer.js
+++ src/redux/reducers/mainReducer.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import boxes from './boxesReducer';
 import dice from './diceReducer';
 import score from './scoreReducer';
+import actions from './actionsReducer';
 
 export default combineReducers({
   boxes,
   dice,
-  score
+  score,
+  actions
 });
